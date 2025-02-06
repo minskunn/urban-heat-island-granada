b05840a4de9179b7d2f7644ebffa0a2fa52a2e2f
minskunn <157802869+minskunn@users.noreply.github.com>
2025-02-06 12:33:52 +0200

    components set up

---
+++ src/components/MapComponent.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+const MapComponent = () => {
+  useEffect(() => {
+    const map = L.map("map").setView([51.505, -0.09], 13); // Example coordinates (London)
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+  }, []);
+
+  return <div id="map" style={{ height: "500px" }}></div>;
+};
+
+export default MapComponent;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <h1>Urban Heat Island Effect</h1>
+    </div>
+  );
+};
+
+export default Sidebar;
